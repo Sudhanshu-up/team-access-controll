f535478c5afa5973e4e22b2e12116c442bd98ab2
Sudhanshu Chaudhari <sudhanshucode36@gmail.com>
2026-08-02 19:24:27 +0530

    Fix import statement to include acceptInvitation in invitation.routes.js

--- backend/src/routes/invitation.routes.js
+++ backend/src/routes/invitation.routes.js
@@ -2,7 +2,7 @@ import  express from "express";
 import { authUser } from "../middlewares/authuser.middleare.js";
 import { acceptInvitationValidator, inviteUserValidator } from "../validator/invitation.validator.js";
 import { validate } from "../middlewares/vaildate.middleware.js";
-import { inviteUser } from "../controllers/invitation.controller.js";
+import { inviteUser,acceptInvitation } from "../controllers/invitation.controller.js";
 
 const router = express.Router();
 
