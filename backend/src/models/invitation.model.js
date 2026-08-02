94148dbcd9ce3565574817755bcbf2e1fda36577
Sudhanshu Chaudhari <sudhanshucode36@gmail.com>
2026-08-02 19:15:03 +0530

    now i hold build reject ,cancell,and get organization invitaions and start building membershipe manegement

--- backend/src/models/invitation.model.js
+++ backend/src/models/invitation.model.js
@@ -29,7 +29,7 @@ const invitationSchema = new mongoose.Schema({
 
     status:{
         type:String,
-        enum:['pending', 'accepted', 'rejected', 'expired'],
+        enum:['pending', 'accepted', 'rejected', 'expired','cancelled'],
         default:'pending',
     },
 
