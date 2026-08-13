c86eab53625a535f2eb9ade74416c151cd282765
Sudhanshu Chaudhari <sudhanshucode36@gmail.com>
2026-08-13 16:51:12 +0530

    style: format server.js for improved readability and consistency

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,18 +3,20 @@ import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 
 dotenv.config();
+
 let PORT = process.env.PORT || 2000;
 
-const startServer = async()=>{
+const startServer = async () => {
     try {
         await connectDb();
-        app.listen(PORT,()=>{
-            console.log(`server running on PORT : ${PORT}`)
-        })
+
+        app.listen(PORT, "0.0.0.0", () => {
+            console.log(`server running on PORT : ${PORT}`);
+        });
+
     } catch (error) {
-        console.log(error.message)
+        console.log(error.message);
     }
 };
 
 startServer();
-
