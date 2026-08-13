22506bcaf3a3ba06255dc0e3cb8c5e3ace614192
Sudhanshu Chaudhari <sudhanshucode36@gmail.com>
2026-08-13 17:29:32 +0530

    style: format app.js for improved readability and consistency

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,26 +1,30 @@
 import express from "express";
 import cors from "cors";
 import userRouter from "./routes/user.routes.js";
-import { cookie } from "express-validator";
 import cookieparser from "cookie-parser";
 import orgnizationRouter from "./routes/organization.routes.js";
 import invitationRouter from "./routes/invitation.routes.js";
-import membershipRouter  from "./routes/membership.routes.js";
+import membershipRouter from "./routes/membership.routes.js";
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieparser());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/auth/users',userRouter);
-app.use('/api/v1/org',orgnizationRouter);
-app.use('/api/v1/invite',invitationRouter);
-app.use('/api/v1/members',membershipRouter);
+app.use("/api/auth/users", userRouter);
+app.use("/api/v1/org", orgnizationRouter);
+app.use("/api/v1/invite", invitationRouter);
+app.use("/api/v1/members", membershipRouter);
 
-
-app.get("/",()=>{
+app.get("/", (req, res) => {
     console.log("hello sudhanshu");
+
+    res.status(200).json({
+        success: true,
+        message: "Team Access Control API is running",
+    });
 });
 
 export default app;
